1a98822ada40439e88d650e211308b37c5e4752c
Dave Harned <dharned@gmail.com>
2018-02-05 21:56:29 -0600

    make automation more test-like

---
+++ test2.js
@@ -0,0 +1,20 @@
+require('chromedriver');
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+(async function example() {
+  let driver = await new Builder().forBrowser('chrome').build();
+  try {
+    await driver.get('http://www.google.com/ncr');
+    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    await driver.sleep(2000);
+  } finally {
+    await driver.getTitle().then( function(title) {
+      if(title === 'webdriver - Google Search') {
+        console.log('Test passed');
+      } else {
+        console.log('Test failed');
+      }
+    });
+    await driver.quit();
+  }
+})();
